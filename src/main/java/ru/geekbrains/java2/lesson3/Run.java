e0ce2baed548d8a713af1321b0491dedcecdd970
vladislav zinchenko <fuuler10@mail.ru>
2021-10-17 21:58:10 +0300

    задание java 2 урок 3

---
+++ src/main/java/ru/geekbrains/java2/lesson3/Run.java
@@ -0,0 +1,22 @@
+package ru.geekbrains.java2.lesson3;
+
+import java.util.*;
+
+public class Run {
+    public static void main(String[] args) {
+        List<String> array =
+                Arrays.asList("Маша", "Даша", "Рома", "Маша", "Даша","Рома",
+                           "Коля", "Лисан", "Рома", "Петя", "Лора");
+
+        Set<String> noDublicate = new LinkedHashSet<>(array);
+        System.out.println("список уникальных слов " + noDublicate);
+
+        HashMap<String, Integer> duplicateNames = new HashMap<>();
+        Integer duplicateName;
+        for (String i : array) {
+            duplicateName = duplicateNames.get(i);
+            duplicateNames.put(i, duplicateName == null ? 1 : duplicateName + 1);
+        }
+        System.out.println("сколько раз встречается каждое слово " + duplicateNames);
+        }
+    }
