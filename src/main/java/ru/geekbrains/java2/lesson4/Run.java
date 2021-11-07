952909bde5c8d976197606cdbf82fd843158c194
vladislav zinchenko <fuuler10@mail.ru>
2021-11-07 19:18:09 +0300

    java 2 dz lesson 4

---
+++ src/main/java/ru/geekbrains/java2/lesson4/Run.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.java2.lesson4;
+
+import javafx.application.Application;
+
+public class Run {
+    public static void main(String[] args) {
+        Application.launch(ChatClient.class, args);
+    }
+}
