38eeeab46e93512ded1c62e7012c7c40c231f429
vladislav zinchenko <fuuler10@mail.ru>
2021-11-08 17:58:16 +0300

    java 2 dz lesson 6

---
+++ src/main/java/ru/geekbrains/java2/lesson6/RunClient.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.java2.lesson6;
+
+public class RunClient {
+    public static void main(String[] args) {
+        SChatClient.run();
+    }
+}
---
+++ src/main/java/ru/geekbrains/java2/lesson6/RunServer.java
@@ -0,0 +1,10 @@
+package ru.geekbrains.java2.lesson6;
+
+/**
+ * @author Vladislav Zinchenko
+ */
+public class RunServer {
+    public static void main(String[] args) {
+        SChatServer.run();
+    }
+}
